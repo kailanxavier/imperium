420f92c0f3348c542b83d48b6dcea29da2c0ed34
Kailan Xavier <kailanxavier@gmail.com>
2026-08-14 09:16:14 +0100

    More mac bash

--- tools/taskradar/taskradar.sh
+++ tools/taskradar/taskradar.sh
@@ -1,7 +1,7 @@
-@echo off
+GREEN='\033[32m'
+NC='\033[0m'
 
-title TaskRadar
-echo Initalising TaskRadar...
-echo Version 0.1.0
+echo "${GREEN}"Initalising TaskRadar..."${NC}"
+echo "${GREEN}"Version 0.1.0"${NC}"
 
 python3 src/main.py
